93cf34a43a80b1b4fc7f48c8a0764701d979ba53
Quang <quangthat97@gmail.com>
2024-04-21 18:02:15 +0700

    first click

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const box = document.querySelector('.box');
+
+let degree = 0;
+
+prev.addEventListener('click', function () {
+	degree += 90;
+	box.style = `transform: perspective(1000px) rotateY(${degree}deg);`;
+});
+
+next.addEventListener('click', function () {
+	degree -= 90;
+	box.style = `transform: perspective(1000px) rotateY(${degree}deg);`;
+});
